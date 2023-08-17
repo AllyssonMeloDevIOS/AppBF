1fa6ec6d76ef39e56ba3b67db41f9d1f4f1bc4ac
Allysson Melo <allymelox@gmail.com>
2023-08-17 15:39:32 -0300

    Criando TabBar

---
+++ AppBackFront/Feature/Home/HomeVC.swift
@@ -0,0 +1,20 @@
+//
+//  HomeVC.swift
+//  AppBackFront
+//
+//  Created by admin on 17/08/23.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+
+    
+
+}
---
+++ AppBackFront/TabBarController/TabBarVC.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarVC.swift
+//  AppBackFront
+//
+//  Created by admin on 17/08/23.
+//
+
+import UIKit
+
+class TabBarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabBar()
+    }
+    
+    private func setupTabBar() {
+        let home = UINavigationController(rootViewController: HomeVC())
+        setViewControllers([home], animated: false)
+        tabBar.isTranslucent = false
+        tabBar.tintColor = UIColor(red: 130/255, green: 26/255, blue: 201/255, alpha: 1.0)
+        tabBar.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
+        
+        guard let items = tabBar.items else { return }
+        
+        items[0].image = UIImage(systemName: "trash")
+    }
+
+}
