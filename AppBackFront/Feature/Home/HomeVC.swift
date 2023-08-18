374a14c314095e55615d596392cd12c2aabba66b
Allysson Melo <allymelox@gmail.com>
2023-08-18 08:46:12 -0300

    Criando HomeScreen

--- AppBackFront/Feature/Home/HomeVC.swift
+++ AppBackFront/Feature/Home/HomeVC.swift
@@ -9,8 +9,16 @@ import UIKit
 
 class HomeVC: UIViewController {
 
+//    var homeScreen : HomeScreen?
+    
+    override func loadView() {
+//        homeScreen = HomeScreen()
+//        view = homeScreen
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .red
     }
     
