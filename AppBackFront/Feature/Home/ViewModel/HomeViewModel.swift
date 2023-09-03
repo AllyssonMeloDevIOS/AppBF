76f6d199f6d6179f532f571e76d4da47b57a6440
Allysson Melo <allymelox@gmail.com>
2023-09-03 08:20:08 -0300

    Criando Lógica na viewModel para service e configurando protocols.

--- AppBackFront/Feature/Home/ViewModel/HomeViewModel.swift
+++ AppBackFront/Feature/Home/ViewModel/HomeViewModel.swift
@@ -7,8 +7,47 @@
 
 import UIKit
 
+protocol HomeViewModelDelegate: AnyObject {
+    func success()
+    func error()
+}
+
+
 class HomeViewModel {
     
+    private let service: HomeService = HomeService()
+    private var nftData: NFTData?
+    weak private var delegate: HomeViewModelDelegate?
     
+    public func delegate(delegate: HomeViewModelDelegate?) {
+        self.delegate = delegate
+    }
     
+    public func fetchRequest(_ typeFetch: TypeFetch) {
+        switch typeFetch{
+        case .mock:
+            service.getHomeFromJason { result, failure in
+                if let result {
+                    print("Sucess")
+                    self.nftData = result
+                    self.delegate?.success()
+                } else {
+                    print(failure as Any)
+                    self.delegate?.error()
+                }
+            }
+        case .request:
+            service.getHome { result, failure in
+                if let result {
+                    print("Sucess")
+                    self.nftData = result
+                    self.delegate?.success()
+                } else {
+                    print(failure as Any)
+                    self.delegate?.error()
+                }
+            }
+            
+        }
+    }
 }
--- AppBackFront/Service/HomeService/HomeService.swift
+++ AppBackFront/Service/HomeService/HomeService.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import Alamofire
 
 protocol HomeServiceDelegate: GenericService {
     func getHomeFromJason(completion: @escaping completion <NFTData?>)
+    func getHome(completion: @escaping completion <NFTData?>)
 }
 
 class HomeService: HomeServiceDelegate {
+    func getHome(completion: @escaping completion<NFTData?>) {
+        let url: String = "https://run.mocky.io/v3/9017b378-45e7-4e41-a6d1-dd9eee60b143"
+        
+        AF.request(url, method: .get).validate(statusCode: 200...299).responseDecodable(of:NFTData.self) { response in
+            debugPrint(response)
+            
+            switch response.result {
+            case .success(let success):
+                print("SUCCESS -> \(#function)")
+            case .failure(let error):
+                print("ERROR -> \(#function)")
+                completion(nil, Error.errorRequest(error))
+            }
+        }
+    }
+    
      
     func getHomeFromJason(completion: @escaping completion <NFTData?>) {
         if let url = Bundle.main.url(forResource: "HomeData", withExtension: "json") {
