d8f093447fcedc9eb81a5ffc1bd0f91a4fe138ba
Allysson Melo <allymelox@gmail.com>
2023-11-08 06:05:28 -0300

    Criação da tela de detalhes.

---
+++ AppBackFront/Feature/NftDetail/ViewModel/NftDetailViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  NftDetailViewModel.swift
+//  AppBackFront
+//
+//  Created by admin on 06/11/23.
+//
+
+import UIKit
+
+class NftDetailViewModel {
+
+    let nft: Nft
+    
+    init(nft: Nft) {
+        self.nft = nft
+    }
+    
+}
